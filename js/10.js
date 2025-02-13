b5f0885cd52a97c9d1183656ad5876e32ca79b05
hyeseon0105 <hyesun0536@gmail.com>
2025-02-13 10:42:38 +0900

    forLoop

--- js/10.js
+++ js/10.js
@@ -8,4 +8,54 @@ $(document).ready(function(){
     {name : '가을', age: 23},
 
   ]
+
+  $("#btnforLoop").on("click",function(){
+    let result = ''
+    // for구문
+    for(let i =0; i < data.length; i++){
+      result = result + `
+       <tr>
+          <td>${i + 1}</td>
+          <td>${data[i].name}</td>
+          <td>${data[i].age}</td>
+         </tr>
+         
+      `
+    }
+    $("#forOut").html(result)
+  })
+
+  $("#btnforEach").on("click",function(){
+    let result = ''
+    // foreach,map구문
+    // data.map
+    data.forEach((x, index)=>{
+      result = result + `
+      <tr>
+         <td>${index + 1}</td>
+         <td>${x.name}</td>
+         <td>${x.age}</td>
+        </tr>
+        
+     `
+    })
+    $("#forEach").html(result)
+  })
+
+  $("#btnJqueryEach").on("click",function(){
+    let result = ''
+    
+    $.each(data,(index,itam)=>{
+      result = result + `
+      <tr>
+         <td>${index + 1}</td>
+         <td>${itam.name}</td>
+         <td>${itam.age}</td>
+        </tr>
+        
+     `
+    })
+    $("#JqueryEach").html(result)
+   
+  })
 })
